f92dc8d6c78d21cb910b415e358221196f397830
George Mamadashvili <georgemamadashvili@gmail.com>
2020-03-13 18:37:16 +0400

    Adds page template

---
+++ page.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<body>
+	<div class="main-block whiten">
+		<?php get_template_part( 'partials/masthead' ); ?>
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+		<div class='container'>
+			<div class="text-container">
+				<h2 class="text-center"><?php the_title(); ?></2>
+
+				<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail( 'post-thumbnail', [ 'class' => 'image' ] ); ?>
+				<?php endif; ?>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+
+		<?php endwhile; endif; ?>
+
+		<?php get_template_part( 'partials/footer' ); ?>
+	</div>
+
+	<?php do_action( 'wp_footer' ); ?>
+</body>
+</html>
