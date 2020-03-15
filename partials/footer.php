41f5b65fc330504a73e9f669aa1bca47514bfe65
George Mamadashvili <georgemamadashvili@gmail.com>
2020-03-15 12:04:58 +0400

    Updates images

--- partials/footer.php
+++ partials/footer.php
@@ -9,6 +9,9 @@
 			</div>
 			<div>
 				<ul class="menu">
+					<li>
+						<a href="https://www.facebook.com/groups/prevencia.ge" class="main-link" target="_blank" rel="noopener noreferrer"><img src="<?php echo Prevencia\the_asset('/img/facebook.png'); ?>" alt="facebook" /></a>
+					</li>
 					<li><a href="<?php echo home_url('/policy'); ?>" class="main-link">პირობები</a></li>
 				</ul>
 			</div>
