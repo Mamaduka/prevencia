6cf100b4b97e3b94dba45cca4383afb09a002e33
George Mamadashvili <georgemamadashvili@gmail.com>
2020-03-15 12:08:28 +0400

    Adds mobile navigation

--- partials/masthead.php
+++ partials/masthead.php
@@ -15,4 +15,31 @@
 			</ul>
 		</div>
 	</div>
+
+	<div style="navigation">
+		<!-- Navigation -->
+		<div class="b-nav">
+			<li><a class="b-link main-link" href="#">მთავარი</a></li>
+			<li><a class="b-link offers-link" href="#">დისტანციური სერვისები</a>
+			</li>
+			<li><a class="b-link recommendations-link" href="#">რეკომენდაციები</a></li>
+			<li><a class="b-link fakenews-link" href="#">ყალბი ამბები</a></li>
+			<li><a class="b-link contact-link" href="#">დაამატე სერვისი</a></li>
+			<li>
+				<a style="margin-left: 30px;" href="https://www.facebook.com/groups/prevencia.ge"  class="main-link" target="_blank" rel="noopener noreferrer">
+					<img src="<?php echo Prevencia\the_asset('/img/facebook.png'); ?>" alt="facebook" />
+				</a>
+			</li>
+		</div>
+
+		<!-- Burger-Icon -->
+		<div class="b-container">
+			<div class="b-menu">
+				<div class="b-bun b-bun--top"></div>
+				<div class="b-bun b-bun--mid"></div>
+				<div class="b-bun b-bun--bottom"></div>
+			</div>
+
+		</div>
+	</div>
 </div>
