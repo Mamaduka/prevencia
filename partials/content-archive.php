6e90d8c09c2494b74e1956a1be263ce7cfc03e1c
George Mamadashvili <georgemamadashvili@gmail.com>
2020-03-23 20:37:53 +0400

    Use medium size images for fake news archvie

--- partials/content-archive.php
+++ partials/content-archive.php
@@ -2,7 +2,7 @@
 	<div class="fake-item">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail( 'post-thumbnail', [ 'class' => 'image' ] ); ?>
+				<?php the_post_thumbnail( 'medium', [ 'class' => 'image' ] ); ?>
 			</a>
 		<?php endif; ?>
 		<p class='date'><?php echo wp_date( 'F j, Y' ); ?></p>
