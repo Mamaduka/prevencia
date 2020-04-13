852ba4bc9b01bf02477ab5fbda7410b400e716cf
George Mamadashvili <georgemamadashvili@gmail.com>
2020-04-13 13:04:38 +0400

    Updates form markup

--- partials/form-markup.php
+++ partials/form-markup.php
@@ -4,8 +4,8 @@
 		<div class="col-md-6">
 			<select name="FORWHO" title="შეავსეთ ველი" id="FORWHO" class="form-control" required>
 				<option selected="selected" disabled>ვისთვის არის სერვისი?</option>
-				<option>ადამიანებისთვის</option>
-				<option>ბიზნესებისთვის</option>
+				<option>B2C</option>
+				<option>B2B</option>
 			</select>
 		</div>
 		<div class="col-md-6">
@@ -15,26 +15,20 @@
 	</div>
 </div>
 
-
 <div class='block'>
 	<p>საკონტაქტო</p>
 	<div class='row'>
 		<div class="col-md-6">
-			<input class="form-control" title="შეავსეთ ველი" id='name' name='NAME' placeholder="სახელი"
-				required />
-
+			<input class="form-control" title="შეავსეთ ველი" id="fullname" name="FULLNAME" placeholder="სახელი, გვარი" required />
 		</div>
 		<div class="col-md-6">
-			<input class="form-control" title="შეავსეთ ველი" id='lastname' name='LASTNAME'
-				placeholder='გვარი' required />
+			<input class="form-control" title="შეავსეთ ველი" id="phone" name="PHONE" placeholder='ტელეფონის ნომერი' required />
 		</div>
 		<div class="col-md-6">
-			<input class="form-control" title="შეავსეთ ველი" id='email' name='EMAIL' placeholder='ელ. ფოსტა'
-				required />
+			<input class="form-control" title="შეავსეთ ველი" id='email' name="EMAIL" placeholder="ელ. ფოსტა" required />
 		</div>
 		<div class="col-md-6">
-			<input class="form-control" title="შეავსეთ ველი" id='phone' name='PHONE'
-				placeholder='ტელეფონის ნომერი' required />
+			<input class="form-control" title="შეავსეთ ველი" id="source" name="SOURCE" placeholder="წყარო" required />
 		</div>
 	</div>
 </div>
@@ -53,28 +47,20 @@
 			</select>
 		</div>
 		<div class="col-md-6">
-			<input class="form-control" title="შეავსეთ ველი" id='SERVICENAME' name='servicename'
-				placeholder='სერვისის დასახელება' required />
+			<input class="form-control" title="შეავსეთ ველი" id="SERVICENAME" name="servicename" placeholder="სერვისის დასახელება" required />
 		</div>
 		<div class="col-md-12">
-			<p>სერვისის/პროდუქტის ნამდვილობის გადასამოწმებლად, გთხოვთ მიუთითოთ ბმული თქვენი ვებგვერდიდან/FB გვერდიდან</p>
-			<input class="form-control" title="შეავსეთ ველი" name='COMPANYSITE' id='companysite'
-				placeholder="www.example.com" required />
-		</div>
-		<div class="col-md-12">
-			<textarea maxlength="600" class="form-control"
-				placeholder="აღწერე სერვისი (მაქსიმუმ 600 სიმბოლო)" title='შეავსეთ ველი' name="SERVICEDESC" id="servicedesc" required></textarea>
+			<textarea maxlength="600" class="form-control" placeholder="აღწერე სერვისი (მაქსიმუმ 600 სიმბოლო)" title='შეავსეთ ველი' name="SERVICEDESC" id="servicedesc" required></textarea>
 		</div>
 		<div class="col-md-6">
-			<input type='file' class="form-control" title="ატვირთე ლოგო" id='logo' name='LOGO' placeholder='ატვირთეთ ლოგო' />
+			<input type='file' class="form-control" title="ატვირთე ლოგო" id="logo" name="LOGO" placeholder="ატვირთეთ ლოგო" />
 			<div class="fake-input">
 				<div class='uploaded'>ატვირთეთ თქვენი ლოგო</div>
 				<span>ატვირთვა</span>
 			</div>
 		</div>
 		<div class="col-md-6">
-			<input class="form-control" title="შეავსეთ ველი" id='duration' name='SERVICEDURATION'
-				placeholder='რა თარიღამდეა სერვისი ხელმისაწვდომი' required />
+			<input class="form-control" title="შეავსეთ ველი" id="duration" name="SERVICEDURATION" placeholder='რა თარიღამდეა სერვისი ხელმისაწვდომი' required />
 		</div>
 
 		<div class="col-md-12">
