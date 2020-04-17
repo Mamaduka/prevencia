cdf6feac71b15c8af55e58114e9c015e38a98344
alex togonidze <alexandertogonidze@gmail.com>
2020-04-17 21:45:23 +0400

    arrow pics small and from assets

--- partials/service-category.php
+++ partials/service-category.php
@@ -10,10 +10,10 @@ $categories = get_terms(['taxonomy' => 'service_category']);
 		<p class="title">სერვისები კატეგორიის მიხედვით</p>
 		<div class="arrow-container">
 			<div class="slick-prev">
-				<img class='a-right control-c next' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Angle_left_font_awesome.svg/1200px-Angle_left_font_awesome.svg.png'>
+				<img class='a-right control-c next' src="<?php echo Prevencia\the_asset('/img/angle-right.png'); ?>">
 			</div>
 			<div class="slick-next">
-				<img class='a-right control-c next' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Angle_right_font_awesome.svg/1200px-Angle_right_font_awesome.svg.png'>
+				<img class='a-right control-c next' src="<?php echo Prevencia\the_asset('/img/angle-left.png'); ?>">
 			</div>
 		</div>
 	</div>
